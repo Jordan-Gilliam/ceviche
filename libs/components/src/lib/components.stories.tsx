aa53288d92e9699217f5095154029024f17def07
Jordan Gilliam <gilliamja.te@gmail.com>
2020-01-11 01:30:02 -0500

    added storybook

---
+++ libs/components/src/lib/components.stories.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+// import { text, number } from '@storybook /addon-knobs';
+import { Components } from './components';
+
+export default { title: 'Button' };
+
+export const primary = () => <Components />;
