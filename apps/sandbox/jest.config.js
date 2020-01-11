6f46dcb9799855b15b0604f92b60d8d61c21eeee
Jordan Gilliam <gilliamja.te@gmail.com>
2020-01-10 23:20:18 -0500

    init

---
+++ apps/sandbox/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'sandbox',
+  preset: '../../jest.config.js',
+  transform: {
+    '^.+\\.[tj]sx?$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'html'],
+  coverageDirectory: '../../coverage/apps/sandbox'
+};
